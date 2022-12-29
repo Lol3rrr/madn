fd34f9d113599b80ab6b3185b1e0fdb14b2143de
Lol3rrr <lol3r@lol3r.net>
2022-12-30 00:43:27 +0100

    Updated IP that is listened on

--- server/src/main.rs
+++ server/src/main.rs
@@ -57,7 +57,7 @@ async fn main() {
         .route("/websocket/:session/:name", get(websocket_handler))
         .with_state(state);
 
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
     tracing::debug!("listening on {}", addr);
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
